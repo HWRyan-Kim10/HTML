37c66d915c0821cc774a6e24a43a780e610ee68e
HWRyan-Kim10 <rkim6@hwemail.com>
2025-11-04 11:37:15 -0800

    more collection troubleshoot

---
+++ public/firebase-config.js
@@ -0,0 +1,13 @@
+// Global Firebase web app configuration
+// Loaded on every page before any Firebase-dependent scripts
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA3rL2zquGcEsovLuhNPyqF2oL7Z2z0aCk",
+  authDomain: "websitehw-3af72.firebaseapp.com",
+  projectId: "websitehw-3af72",
+  storageBucket: "websitehw-3af72.firebasestorage.app",
+  messagingSenderId: "292748797297",
+  appId: "1:292748797297:web:da4926448ac833c2c002d0",
+  measurementId: "G-J2KS8NGMSR"
+};
+
+
